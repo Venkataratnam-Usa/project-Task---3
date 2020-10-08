bf9425fab021c8b25df7ef58cec2ba5b8b6cbb51
Venkataratnam-Usa <71263889+Venkataratnam-Usa@users.noreply.github.com>
2020-10-08 08:42:00 +0530

    Add files via upload

---
+++ precache-manifest.2eb21a45544502327d91e026ca83bd24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66a9625b4887b4704406cb32660953d",
+    "url": "/reactproject3/index.html"
+  },
+  {
+    "revision": "cf4c646e04b47c2131c8",
+    "url": "/reactproject3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2ef07c0739cc28e1022",
+    "url": "/reactproject3/static/js/2.3a185f53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactproject3/static/js/2.3a185f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf4c646e04b47c2131c8",
+    "url": "/reactproject3/static/js/main.c876893b.chunk.js"
+  },
+  {
+    "revision": "73080121c56fe137e18b",
+    "url": "/reactproject3/static/js/runtime-main.c3bf1e21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactproject3/static/media/logo.5d5d9eef.svg"
+  }
+]);
